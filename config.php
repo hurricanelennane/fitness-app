11c5964308841e7ab002941947fdb11882501b59
dylanjtastet <dylanjtastet@gmail.com>
2017-11-28 02:11:16 -0500

    Fully functional secure login bois lets gooo

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+ob_start();
+session_start();
+date_default_timezone_set('America/New_York');
+
+//db credentials
+define("DB_HOST","localhost:8889");
+define("DB_USER","gainz_app");
+define("DB_PASS","i_heart_kmp");
+define("DB_NAME","gainz_db");
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if($conn -> connect_errno){
+	echo "db error: ".$conn -> connect_error;
+	exit();
+}
+?>
